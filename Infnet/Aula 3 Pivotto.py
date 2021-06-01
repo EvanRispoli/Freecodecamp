03dfdb01d3c9438a7224c197f9857b9ca20d88f4
Evan Rispoli <evan.rispoli@gmail.com>
2021-06-01 01:45:02 -0400

    final

---
+++ Infnet/Aula 3 Pivotto.py
@@ -0,0 +1,4 @@
+print('Ola! Sejam bem-vindos ao instituto Infnet')
+print('Informe o seu nome: ')
+# Vamos solicitar que o usuario do prorama informe seu nome. Para isso, vamos usar a funcao input, que e a funcao padrao de entrada de dados em python
+nome = input()
